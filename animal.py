1276a11b9c96eab5e58cef9f5448af0c8194c170
Me <Me@me>
2025-10-30 14:01:06 +1030

    # class HealthRecords # class Animal
    Added the healthrecords
    and check to ensure
    valid inputs. Started
    to create the parent
    Animal class

--- animal.py
+++ animal.py
@@ -7,3 +7,29 @@ ID: 110448094
 Username: chuey008
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+# Imports are used to show a timestamp of the health events and records and typing to do checks within Health
+from datetime import date
+from typing import List
+
+class HealthRecord:
+    def __init__(self, description, reported_on, severity, treatment):
+        """
+        Represents a health record in an animal if there is an event.
+        All are private to enforce encapsulation and checks to see the validity of  to prevent invalid
+        records from being entered.
+        """
+        if not isinstance(description, str) or not description.strip():
+            raise ValueError("Description cannot be an empty string.")
+        self.description =
+
+class Animal:
+    def __init__(self, name, species, age, diet):
+        """
+        Initialise an Animal object with name, species, age and dietary attributes.
+        """
+        self.__validate_init(name, species, age, diet)
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
