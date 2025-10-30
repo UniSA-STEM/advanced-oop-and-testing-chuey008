c0a8937b041ebcad7d855fdf61ccc5b4bbd9a2e3
Me <Me@me>
2025-10-30 14:50:48 +1030

    # fixed code and added # getter
    Using @property to use
    to change a method to
    a getter

--- animal.py
+++ animal.py
@@ -13,15 +13,43 @@ from datetime import date
 from typing import List
 
 class HealthRecord:
-    def __init__(self, description, reported_on, severity, treatment):
+    def __init__(self, description, reported_on, severity, treatment_notes):
         """
         Represents a health record in an animal if there is an event.
-        All are private to enforce encapsulation and checks to see the validity of  to prevent invalid
+        All are private to enforce encapsulation and checks to see the validity of HealthRecords to prevent invalid
         records from being entered.
         """
         if not isinstance(description, str) or not description.strip():
             raise ValueError("Description cannot be an empty string.")
-        self.description =
+        if not isinstance(reported_on, date):
+            raise ValueError("Reported on must be a date and time.")
+        if not isinstance(severity, int) or not (1 <= severity <=10):
+            raise ValueError("Severity must be an integer between 1 and 10.")
+        if not isinstance(treatment_notes, str):
+            raise ValueError("Treatment notes must be a string.")
+
+        # ensuring internal fields are private
+        self.__description = description
+        self.__reported_on = reported_on
+        self.__severity = severity
+        self.__treatment_notes = treatment_notes
+
+    # using @property decorator to transform a method into a getter
+    @property
+    def description(self):
+        return self.__description
+
+    @property
+    def report_on(self):
+        return self.__reported_on
+
+    @property
+    def severity(self):
+        return self.__severity
+
+    @property
+    def treatment_notes(self):
+        return self.__treatment_notes
 
 class Animal:
     def __init__(self, name, species, age, diet):
