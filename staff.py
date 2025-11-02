a4022a4bcfdeb7c7089da42f0d75256fdedba1b8
Me <Me@me>
2025-11-02 12:43:37 +1030

    # staff class
    Started to complete the
    staff class. May need to
    import stuff from animal
    class like healthrecord
    and animal. Made a note for
    later. Completed some
    private validation checks
    to make sure it checks out.

--- staff.py
+++ staff.py
@@ -7,3 +7,30 @@ ID: 110448094
 Username: chuey008
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+# will need to import from animal
+
+class Staff:
+    def __init__(self, staff_id, name, role):
+        """
+        This class represents a staff member who has tasks depending on their role.
+        Private attributes will continue to be used for encapsulation and ensuring no changes are made.
+        Adding validation checks to make sure what is entered is true.
+        Checks to see if staff_id and name is a string and not empty.
+        Role must only be either vet or zookeeper.
+        """
+        self.__validate_init(staff_id, name, role)
+        self.__staff_id = staff_id
+        self.__role = role
+
+    def __validate_init(self, staff_id, name, role):
+        """
+        Internal validator to help conduct checks for testing.
+        """
+        if not isinstance(staff_id, str) or not staff_id.strip():
+            raise ValueError("Staff ID cannot be an empty string.")
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError("Name cannot be an empty string.")
+        if role not in ("Veterinarian", "Zookeeper"):
+            raise ValueError("Role must be either 'Veterinarian' or 'Zookeeper'.")
+
