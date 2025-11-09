6c7e5e32764ff98f2b82bdb50adee66011c61270
Me <Me@me>
2025-11-09 14:07:12 +1030

    # EnvrionmentType
    Created a class for environmenttype
    used enumerate to help invalid
    environment types that are not on
    the specified list otherwise, it
    will be under the temperate environ

--- enclosure.py
+++ enclosure.py
@@ -26,6 +26,7 @@ class EnvrionmentType(Enum):
     TROPICAL = "tropical"
     RAINFOREST = "rainforest"
     ARCTIC = "arctic"
+    TEMPERATE = "temperate"  # general environment that is able to host a wide range of animals
 
 class Enclosure:
     """
@@ -56,8 +57,8 @@ class Enclosure:
             raise ValueError("name cannot be an empty string.")
         if not isinstance(size_sqm, int) or size_sqm < 0:
             raise ValueError("size_sqm must be a positive integer")
-        if not isinstance(environment, str):  # will need to find a way to check for environment type
-            raise
+        if not isinstance(environment, EnvironmentType):
+            raise TypeError("environment must be a valid EnvironmentType")
         if not isinstance(capacity, int) or capacity <= 0:
             raise ValueError("capacity must be a positive integer")
 
@@ -67,7 +68,28 @@ class Enclosure:
         """
         self.__cleanliness = max(0, self.__cleanliness - amount)
 
-    def __compatible_with_environment(self, animal):
+    def __compatible_with_environment(self, animal: Animal):
         """
         Private helper to check if the animal is compatible with the environment type in the enclosure.
+        Method encapsulates the mapping of animal species to environments.
+        Substring based, for additional flexibility - e.g. specific species like "brown bear" and "polar bear"
         """
+        a_s = animal.species.lower()
+        env = self.__environment
+        if env == EnvironmentType.AQUATIC:
+            return any(i in a_s for i in ("dolphin", "seal", "penguin", "fish", "sea otter", "turtle"))
+        if env == EnvironmentType.SAVANNAH:
+            return any(i in a_s for i in ("giraffe", "elephant", "zebra", "ostrich", "red kangaroo", "meerkat"))
+        if env == EnvironmentType.DESERT:
+            return any(i in a_s for i in ("camel", "scorpion", "dingo", "bearded dragon", "hawk", "cobra"))
+        if env == EnvironmentType.TROPICAL:
+            return any(i in a_s for i in ("lemur", "green anaconda", "parrot", "gecko", "capybara", "sloth"))
+        if env == EnvironmentType.RAINFOREST:
+            return any(i in a_s for i in ("tree frog", "toucan", "spider monkey", "harpy eagle", "tiger", "gorilla"))
+        if env == EnvironmentType.ARCTIC:
+            return any(i in a_s for i in ("polar bear", "arctic fox", "snowy owls", "walrus", "seal", "puffin"))
+        if env == EnvironmentType.TEMPERATE:
+            # temperate accepts generalists type animal species
+            return True
+        return False
+
