5566b25ca03db396907b4f83a7d1f576400d6223
Me <Me@me>
2025-11-16 20:15:08 +1030

    # main
    Creating a function to
    handle the demonstration
    and testing of the 3
    Classes scripts

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ ID: 110448094
 Username: chuey008
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+from animal import Mammal, Bird, Reptile, Other
+from enclosure import Enclosure, EnvironmentType
+from staff import Staff
+
+def demo():
+    """
+    Run a short scenario to demonstrate the below:
+    1. create animals for Mammal, Bird, Reptile and Other subclasses
+    2. create enclosures and compatible animals based on EnvironmentType
+    3. create staff and assign responsibilities based on role
+    4. demonstrate feeding, cleaning, vet checks, error cases (e.g. moving an animal under treatment)
+    """
+    # create animals using private state, public API used below
+    ellie = Mammal(name="Ellie", species="elephant", age=5, diet="Herbivore")
+    peta = Bird(name="Peta", species="parrot", age=2, diet="Omnivore")
+    tilly = Reptile(name="Tilly", species="turtle", age=25, diet="Omnivore")
+    fred = Other(name="Fred", species="tree frog", age=10, diet="Carnivore")
+
